d161da64f96feecdc751f46a9864b13637fea76c
zhangkai <deyang143@126.com>
2020-02-20 00:35:16 +0800

    rename protocol

--- Example/ZKFoundation/ZKCollectionViewCell.h
+++ Example/ZKFoundation/ZKCollectionViewCell.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ZKCollectionViewCell : UICollectionViewCell <ZKCollectionViewHelperInjectionDelegate>
+@interface ZKCollectionViewCell : UICollectionViewCell <ZKCollectionViewAdapterInjectionDelegate>
 
 @end
 
