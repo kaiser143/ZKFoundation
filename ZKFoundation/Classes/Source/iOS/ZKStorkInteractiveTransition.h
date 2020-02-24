652dabf02a70b9f5e8d8f00038c003a436f1b245
zhangkai <deyang143@126.com>
2020-02-24 22:06:58 +0800

    update

---
+++ ZKFoundation/Classes/Source/iOS/ZKStorkInteractiveTransition.h
@@ -0,0 +1,48 @@
+//
+//  ZKStorkInteractiveTransition.h
+//  ZKFoundation
+//
+//  Created by zhangkai on 2019/11/14.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+/*!
+ *  效果参考iOS13 controller.modalPresentationStyle = UIModalPresentationAutomatic
+ *  @code
+        ModalViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"ModalViewController"];
+        controller.modalPresentationStyle = UIModalPresentationCustom;
+
+        self.animator = [[ZKStorkInteractiveTransition alloc] initWithModalViewController:modalVC];
+        self.animator.transitionDuration = 0.6f;
+
+        if (self.scrollViewSwitch.isOn) {
+            [self.animator setContentScrollView:controller.scrollView];
+        }
+
+        modalVC.transitioningDelegate = self.animator;
+        [self presentViewController:controller animated:YES completion:nil];
+ *  @endcode
+ */
+@interface ZFDetectScrollViewEndGestureRecognizer : UIPanGestureRecognizer
+@property (nonatomic, weak) UIScrollView *scrollview;
+@end
+
+@interface ZKStorkInteractiveTransition : UIPercentDrivenInteractiveTransition <UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate, UIGestureRecognizerDelegate>
+
+@property (nonatomic, assign, getter=isDragable) BOOL dragable; // 是否支持手势dismiss controller
+@property (nonatomic, readonly) ZFDetectScrollViewEndGestureRecognizer *gesture;
+@property (nonatomic, assign) UIGestureRecognizer *gestureRecognizerToFailPan;
+@property BOOL bounces;
+@property CGFloat behindViewScale;
+@property CGFloat behindViewAlpha;
+@property CGFloat transitionDuration;
+@property CGFloat cornerRadius;         // default 10
+@property CGFloat translateForDismiss;  // default 200
+
+- (id)initWithModalViewController:(UIViewController *)modalViewController;
+- (void)setContentScrollView:(UIScrollView *)scrollView;
+
+@end
