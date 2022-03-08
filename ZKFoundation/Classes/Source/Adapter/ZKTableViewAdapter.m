e54dd5f19dcfe0c951b884f41067577b23d92aab
zhangkai <deyang143@126.com>
2022-03-08 17:01:10 +0800

    upddate

--- ZKFoundation/Classes/Source/Adapter/ZKTableViewAdapter.m
+++ ZKFoundation/Classes/Source/Adapter/ZKTableViewAdapter.m
@@ -581,9 +581,7 @@ CGFloat ZKAutoHeightForHeaderFooterView = -1;
 - (id)currentSectionModel:(NSInteger)section {
     id currentModel = nil;
     if (section < self.data.count) {
-        NSArray *arr = [self.data objectAtIndex:section];
-        if (arr.count)
-            currentModel = [arr objectAtIndex:0];
+        currentModel = [self.data objectAtIndex:section];
     }
     return currentModel;
 }
